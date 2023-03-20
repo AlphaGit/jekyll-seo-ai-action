d51703743efed6ad9340b13116399f822e25c6b2
Alpha <alphagma@gmail.com>
2023-03-19 21:05:51 -0400

    Repackage as module

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const core = require('@actions/core');
+
 const files = require('./src/changed-files.js');
 const report = require('./src/report.js');
 const gitHub = require('./src/github.js');
 const generator = require('./src/generator.js');
 
 const run = async () => {
-    const changedFiles = await files.getChangedFiles();
-    const results = await generator.generateDescriptions(changedFiles);
-    const reportContent = await report.generateReport(results);
+    try {
+        const changedFiles = await files.getChangedFiles();
+        const results = await generator.generateDescriptions(changedFiles);
+        const reportContent = await report.generateReport(results);
 
-    await gitHub.createCommit(changedFiles);
-    await gitHub.createComment(reportContent);
+        await gitHub.createCommit(changedFiles);
+        await gitHub.createComment(reportContent);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 };
 
 run();
