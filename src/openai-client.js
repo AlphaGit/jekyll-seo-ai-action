5d8b1953aa6d887cf96d0122070f54d967d21d11
Alpha <alphagma@gmail.com>
2023-03-19 22:13:15 -0400

    Switch ncc for esbuild

--- src/openai-client.js
+++ src/openai-client.js
@@ -1,4 +1,4 @@
-import { Configuration, OpenAIApi } from 'openai-api';
+import { Configuration, OpenAIApi } from 'openai';
 import { OPENAI_KEY } from './settings';
 
 export const getModelResponse = async (prompt) => {
