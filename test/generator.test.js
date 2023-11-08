64c5fbd45f191bb20451794e11f098378cce63ce
Alpha <alphagma@gmail.com>
2023-11-07 20:44:00 -0500

    Refactor generateDescriptions function tests

--- test/generator.test.js
+++ test/generator.test.js
@@ -22,52 +22,61 @@ const { getModelResponse } = await import("../src/openai-client");
 const fs = await import("fs/promises");
 
 describe("generateDescriptions", () => {
-    it("should return an empty array when given an empty array", async () => {
-        const result = await generateDescriptions([]);
-        expect(result).toEqual([]); // No changes needed as the input is an empty array
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
-    it("should return an empty array when given an array of non-existent files", async () => {
-        fs.readFile.mockRejectedValue(new Error("File not found"));
-        const result = await generateDescriptions(["non-existent-file.md"]);
-        expect(result).toEqual([new DescriptionResult("non-existent-file.md", null, GenerationStatus.UNKNOWN_ERROR, "Error generating description for non-existent-file.md: File not found")]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/empty.md", null, GenerationStatus.UNKNOWN_ERROR, "No front matter found for test/fixtures/empty.md -- is it really a blog post?")]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/with-description.md", "Existing description", GenerationStatus.SKIPPED, "Front matter already contains a description for test/fixtures/with-description.md -- skipping")]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/without-front-matter.md", null, GenerationStatus.UNKNOWN_ERROR, "No front matter found for test/fixtures/without-front-matter.md -- is it really a blog post?")]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/without-date.md", null, GenerationStatus.UNKNOWN_ERROR, "No date in front matter for test/fixtures/without-date.md")]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/with-description.md", "This is a test description.", GenerationStatus.SKIPPED, "Front matter already contains a description for test/fixtures/with-description.md -- skipping")]);
+    describe('empty file list' , () => {
+        it("should return an empty array when given an empty array", async () => {
+            const result = await generateDescriptions([]);
+            expect(result).toEqual([]);
+        });
     });
 
-    it("should return an empty array when the files do not have front matters", async () => {
-        fs.readFile.mockResolvedValueOnce(`\n\nBody`);
-        const result = await generateDescriptions(["test/fixtures/without-front-matter.md"]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/without-front-matter.md", null, GenerationStatus.UNKNOWN_ERROR, "Error: No front matter found")]);
-    });
-
-    it("should return an empty array when the files do not have a date in the front matter", async () => {
-        fs.readFile.mockResolvedValueOnce(`---\n---\n\nBody`);
-        const result = await generateDescriptions(["test/fixtures/without-date.md"]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/without-date.md", null, GenerationStatus.UNKNOWN_ERROR, "Error: No date in front matter")]);
-    });
-
-    it("should return an empty array when the files already have descriptions in the front matter", async () => {
-        fs.readFile.mockResolvedValueOnce(`---\ndate: 2021-01-01\ndescription: This is a test description.\n---\n\nBody`);
-        const result = await generateDescriptions(["test/fixtures/with-description.md"]);
-        expect(result).toEqual([new DescriptionResult("test/fixtures/with-description.md", "This is a test description.", GenerationStatus.SKIPPED, "Description already present")]);
-    });
-
-    it("should return an array of results when given an array of files that don't contain a description", async () => {
-        fs.readFile.mockResolvedValueOnce(`---\ndate: 2021-01-01\n---\n\nBody`);
-        getModelResponse.mockResolvedValueOnce("This is a generated test description.");
-        const result = await generateDescriptions(["test/fixtures/without-description.md"]);
-        expect(result).toEqual([
-            new DescriptionResult("test/fixtures/without-description.md", "This is a generated test description.", GenerationStatus.GENERATED, ""),
-        ]);
-        expect(fs.writeFile).toHaveBeenCalledWith(
-            "test/fixtures/without-description.md",
-            `---\ndate: 2021-01-01T00:00:00.000Z\ndescription: This is a generated test description.\n---\n\n\nBody`,
-            "utf-8"
-        );
+    describe('errors', () => {
+        it("should return an empty array when given an array of non-existent files", async () => {
+            fs.readFile.mockRejectedValue(new Error("File not found"));
+            const result = await generateDescriptions(["non-existent-file.md"]);
+            expect(result).toEqual([new DescriptionResult("non-existent-file.md", null, GenerationStatus.UNKNOWN_ERROR, "File not found")]);
+        });
+    
+        it("should the existing files that already contain a description", async () => {
+            fs.readFile.mockResolvedValueOnce(`---\ntitle: Some title\ndescription: Some description\n---\n\nBody`);
+            const result = await generateDescriptions(["test/fixtures/with-description.md"]);
+            expect(result).toEqual([new DescriptionResult("test/fixtures/with-description.md", "Some description", GenerationStatus.SKIPPED)]);
+        });
+    
+        it("should return an empty array when the files do not have front matters", async () => {
+            fs.readFile.mockResolvedValueOnce(`\n\nBody`);
+            const result = await generateDescriptions(["test/fixtures/without-front-matter.md"]);
+            expect(result).toEqual([new DescriptionResult("test/fixtures/without-front-matter.md", null, GenerationStatus.UNKNOWN_ERROR, "No front matter found")]);
+        });
+    
+        it("should return an empty array when front matter is empty", async () => {
+            fs.readFile.mockResolvedValueOnce(`---\n---\nBody`);
+            const result = await generateDescriptions(["test/fixtures/without-front-matter.md"]);
+            expect(result).toEqual([new DescriptionResult("test/fixtures/without-front-matter.md", null, GenerationStatus.UNKNOWN_ERROR, "No front matter found")]);
+        });
+    
+        it("should return an empty array when the files already have descriptions in the front matter", async () => {
+            fs.readFile.mockResolvedValueOnce(`---\ndate: 2021-01-01\ndescription: This is a test description.\n---\n\nBody`);
+            const result = await generateDescriptions(["test/fixtures/with-description.md"]);
+            expect(result).toEqual([new DescriptionResult("test/fixtures/with-description.md", "This is a test description.", GenerationStatus.SKIPPED)]);
+        });
+    
+        it("should return an array of results when given an array of files that don't contain a description", async () => {
+            fs.readFile.mockResolvedValueOnce(`---\ndate: 2021-01-01\n---\n\nBody`);
+            getModelResponse.mockResolvedValueOnce("This is a generated test description.");
+            const result = await generateDescriptions(["test/fixtures/without-description.md"]);
+            expect(result).toEqual([
+                new DescriptionResult("test/fixtures/without-description.md", "This is a generated test description.", GenerationStatus.GENERATED, ""),
+            ]);
+            expect(fs.writeFile).toHaveBeenCalledWith(
+                "test/fixtures/without-description.md",
+                `---\ndate: 2021-01-01T00:00:00.000Z\ndescription: This is a generated test description.\n---\n\n\nBody`,
+                "utf-8"
+            );
+        });
     });
 
     it("should ignore separators in the body", async () => {
